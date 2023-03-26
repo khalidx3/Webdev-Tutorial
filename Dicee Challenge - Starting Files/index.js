54741325da8d5d87823d39464d090dd30af1b5a6
Khalid <khalidhasanx3@gmail.com>
2023-03-26 21:27:11 +0530

    Upadte

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+ 
+var images1 = "images/dice"+ randomNumber1 +".png";
+var images2 = "images/dice"+ randomNumber2 +".png";
+ 
+document.querySelector(".dice .img1").setAttribute("src",images1);
+document.querySelector(".dice .img2").setAttribute("src",images2);
+
+if(randomNumber1==randomNumber2){
+    document.querySelector(".container h1").innerHTML="Draw!!!!";
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector(".container h1").innerHTML="Player 1 won it";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector(".container h1").innerHTML="Player 2 won it";
+}
